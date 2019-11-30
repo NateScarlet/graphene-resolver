428c75a3591b5f51852e346be7348fb24e1e9c52
NateScarlet <NateScarlet@Gmail.com>
2019-11-30 16:49:38 +0800

    chore(release): 0.1.1

--- graphene_resolver/__init__.py
+++ graphene_resolver/__init__.py
@@ -1,6 +1,6 @@
 """Using mongoose-like schema to write apollo-like resolver for graphene.  """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 from .resolver import Resolver
 from . import connection, typedef
 from .schema import CONFIG_PROCESSOR
